765a787604f7f18cbed4bc6e91a85d830da56246
Jiang Zhengyi <1556268049@qq.com>
2025-05-29 16:09:59 +0800

    Create git_pull.sh

---
+++ shell/git_pull.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -d "/data/jzy/fastllm" ]; then
+    cd /data/jzy/fastllm
+elif [ -d "/root/fastllm" ]; then
+    cd /root/fastllm
+fi
+
+username="CoderJzy"
+password=""
+
+expect << EOF
+    set env(https_proxy) "192.168.1.170:10809"
+    
+    spawn git fetch
+    
+    expect "Username for 'https://github.com':"
+    send "$username\r"
+    
+    expect "Password for 'https://$username@github.com':"
+    send "$password\r"
+    
+    expect eof
+    
+    spawn git rebase
+    expect eof
+EOF
