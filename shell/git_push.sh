d0ed5a30de60dd7ca14b0abd6b3d81811be3f3cb
Jiang Zhengyi <1556268049@qq.com>
2025-05-29 16:09:06 +0800

    Update git_push.sh

--- shell/git_push.sh
+++ shell/git_push.sh
@@ -1,23 +1,30 @@
-#!/usr/bin/expect
+#!/bin/bash
 
-set username "CoderJzy"
-set password "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"
-set env(https_proxy) "192.168.1.170:10809"
+if [ -d "/data/jzy/fastllm" ]; then
+    cd /data/jzy/fastllm
+elif [ -d "/root/fastllm" ]; then
+    cd /root/fastllm
+fi
 
-cd /data/jzy/fastllm
+username="CoderJzy"
+password=""
 
-spawn git add . 
-expect eof
-
-spawn git commit -m update
-expect eof
-
-spawn git push
-
-expect "Username for 'https://github.com':"
-send "$username\r"
-
-expect "Password for 'https://$username@github.com':"
-send "$password\r"
-
-expect eof
+expect << EOF
+    set env(https_proxy) "192.168.1.170:10809"
+    
+    spawn git add . 
+    expect eof
+    
+    spawn git commit -m update
+    expect eof
+    
+    spawn git push
+    
+    expect "Username for 'https://github.com':"
+    send "$username\r"
+    
+    expect "Password for 'https://$username@github.com':"
+    send "$password\r"
+    
+    expect eof
+EOF
